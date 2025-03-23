36e328c528a0fff8efceb60b8ae6cce1fdaaf732
Rahul raj <42221167+Rahulrajsit@users.noreply.github.com>
2025-03-23 16:17:40 +0530

    Create Abstract.cpp

---
+++ Abstract.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class A{                // Abstract class and obj you can not create for this.
+public:
+    virtual void getinfo()=0;  // Pure Virtual Class
+};
+class B:public A{
+public:
+    void getinfo(){
+        cout<<"Hi from Raj";
+    }
+};
+int main(){
+    B obj;
+    obj.getinfo();
+
+    //A obj1;      // Not possible for abstract class
+    return 0;
+}
